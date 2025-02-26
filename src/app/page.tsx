ddb114ed4d37444236713afc89586765bd4de3b3
Katherine Roughan <46721827+roughk@users.noreply.github.com>
2025-02-26 02:02:01 -0600

    testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,38 @@
-import React from 'react';
 
-export default function Home() {
-  return (
-    <div>
-      <h1>Welcome to My Website</h1>
-      <p>This is the home page of my Next.js application.</p>
-    </div>
-  );
+import { Metadata } from "next";
+import fs from "fs";
+import path from "path";
+import { DataTable } from "./data-table-components/data-table";
+import { columns } from "./data-table-components/columns";
+
+export const metadata: Metadata = {
+    title: "Expenses",
+    description: "A Expense tracker build using Tanstack Table."
+};
+
+async function getData() {
+    const filePath = path.join(
+        process.cwd(),
+        "src/app/data-table-components",
+        "data.json"
+    );
+    const data = fs.readFileSync(filePath, "utf8");
+    return JSON.parse(data);
 }
+
+export default async function Page() {
+    const data = await getData();
+    console.log("data", data);
+
+    return (
+        <div className="h-full flex-1 flex-col space-y-2 p-8 md:flex">
+            <div className="flex items-center justify-between">
+                <p className="text-muted-foreground">
+                    Here&apos;s a list of your expenses for this month!
+                </p>
+            </div>
+            <DataTable data={data} columns={columns} />
+        </div>
+    );
+}
+
